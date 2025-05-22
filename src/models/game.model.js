3691497b7947b2ba08fe66b25e48acfacf49e07f
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2025-05-22 07:53:30 -0300

    Initial commit

---
+++ src/models/game.model.js
@@ -0,0 +1,40 @@
+import prisma from "../../prisma/prisma.js";
+
+class GameModel {
+  // Obter todos os jogos
+  async findAll(name, platform) {
+    const where = {};
+
+    if (name) {
+      where.name = {
+        contains: name,
+      };
+    }
+
+    if (platform) {
+      where.platform = {
+        contains: platform,
+      };
+    }
+
+    const games = await prisma.game.findMany({
+      where,
+    });
+
+    return {
+      total: games.length,
+      games,
+    };
+  }
+
+  // Criar um novo jogo
+  async create(data) {
+    const game = await prisma.game.create({
+      data,
+    });
+
+    return game;
+  }
+}
+
+export default new GameModel();
